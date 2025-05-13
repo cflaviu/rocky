8d5e7be6b4ccb34c392fc7a42342a0d599ef8133
Flaviu <cflaviu@users.noreply.github.com>
2025-05-13 12:38:16 +0300

    STB conditional link

--- src/rocky/LinkStb.cpp
+++ src/rocky/LinkStb.cpp
@@ -1,4 +1,7 @@
-#define STB_TRUETYPE_IMPLEMENTATION
-#define STB_RECT_PACK_IMPLEMENTATION
-#include "imstb_rectpack.h"
-#include "imstb_truetype.h"
+#include "Version.h"
+#ifdef ROCKY_HAS_IMGUI
+    #define STB_TRUETYPE_IMPLEMENTATION
+    #define STB_RECT_PACK_IMPLEMENTATION
+    #include "imstb_rectpack.h"
+    #include "imstb_truetype.h"
+#endif
